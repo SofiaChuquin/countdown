7d680eecb093b3aca7ae3144f1cf6902cb025779
sofiachuquin <sofiachuquin@gmail.com>
2017-10-29 22:12:34 -0500

    Stablish deadline

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deadline: 'October 31, 2017' }
+  }
+
   render() {
     return (
       <div className='app'>
-        <div className='app__title'>Countdown to October 31, 2017</div>
+        <div className='app__title'>Countdown to {this.state.deadline}</div>
         <div className='app__date'>
           <div className='app__date__item'>14 days</div>
           <div className='app__date__item'>30 hours</div>
